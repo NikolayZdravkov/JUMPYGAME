d086bdf266bfe362b68ae2c2dc9c21d4d8a28525
niketzaBG <zdravkovnikolay5@gmail.com>
2024-06-14 20:36:14 +0300

    Added running and jumping animations and code

--- scripts/player.cs
+++ scripts/player.cs
@@ -6,6 +6,7 @@ public partial class player : CharacterBody2D
 	public const float Speed = 130.0f;
 	public const float JumpVelocity = -300.0f;
 
+
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
